8fcd3d83e86234aa4a629b194faeded309b1db1b
wxx <917660818@qq.com>
2023-10-31 08:24:18 +0800

    add aerial and delete tagbar

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local cmd = vim.api.nvim_create_user_command
+local namespace = vim.api.nvim_create_namespace
+
 vim.cmd("hi LineNrAbove guifg=#cc6666 ctermfg=red")
 vim.cmd("hi LineNrBelow guifg=#66cc66 ctermfg=green")
 vim.cmd("hi Normal ctermbg=none")
@@ -8,3 +13,35 @@ vim.cmd("hi SignColumn ctermbg=none")
 vim.cmd("hi VertSplit guifg=none guibg=none")
 vim.cmd("hi TelescopeMatching guifg=none guibg=grey")
 vim.cmd("hi! link Visual Search")
+
+autocmd("BufEnter", {
+  desc = "Quit AstroNvim if more than one window is open and only sidebar windows are list",
+  group = augroup("auto_quit", { clear = true }),
+  callback = function()
+    local wins = vim.api.nvim_tabpage_list_wins(0)
+    -- Both neo-tree and aerial will auto-quit if there is only a single window left
+    if #wins <= 1 then
+      return
+    end
+    local sidebar_fts = { aerial = true, ["neo-tree"] = true }
+    for _, winid in ipairs(wins) do
+      if vim.api.nvim_win_is_valid(winid) then
+        local bufnr = vim.api.nvim_win_get_buf(winid)
+        local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+        -- If any visible windows are not sidebars, early return
+        if not sidebar_fts[filetype] then
+          return
+        -- If the visible window is a sidebar
+        else
+          -- only count filetypes once, so remove a found sidebar from the detection
+          sidebar_fts[filetype] = nil
+        end
+      end
+    end
+    if #vim.api.nvim_list_tabpages() > 1 then
+      vim.cmd.tabclose()
+    else
+      vim.cmd.qall()
+    end
+  end,
+})
