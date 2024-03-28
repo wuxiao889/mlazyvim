148f89ccb47fc04d65d7206b700b9854240c7428
wxx <917660818@qq.com>
2024-03-28 11:45:40 +0800

    cmp buffer for all visiable buffers

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -45,6 +45,27 @@ return {
       }
       opts.mapping = cmp.mapping.preset.insert(mapping)
       opts.completion = { completeopt = "menu,menuone,noinsert,noselect" }
+      opts.sources = {
+        {name = "path"},
+        {name = "nvim_lsp"},
+        {
+          name = "buffer",
+          option = {
+            get_bufnrs = function()
+              local buf = vim.api.nvim_get_current_buf()
+              local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
+              if byte_size > 1024 * 1024 then -- 1 Megabyte max
+                return {}
+              end
+              local bufs = {}
+              for _, win in ipairs(vim.api.nvim_list_wins()) do
+                bufs[vim.api.nvim_win_get_buf(win)] = true
+              end
+              return vim.tbl_keys(bufs)
+            end,
+          },
+        },
+      }
     end,
   },
 
--- lua/plugins/interestingwords.lua
+++ lua/plugins/interestingwords.lua
@@ -1,5 +1,6 @@
 return {
   "lfv89/vim-interestingwords",
+  -- keys will overwrite default keymaps
   keys = {
     { "<leader>k", ":call InterestingWords('n')<cr>", desc = "InterestingWords"},
     { "<leader>K", ":call UncolorAllWords()<cr>", desc = "UncolorAllWords" }
